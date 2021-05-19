c7664ae79ee86afcb88ff1651994940bdbcb3b09
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 14:53:24 -0500

    retest

--- shell-steps.sh
+++ shell-steps.sh
@@ -1,6 +1,6 @@
 mkdir /home/bitnami/oneninescripts && cd /home/bitnami/oneninescripts
 wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
-wget -q https://github.com/mukeshwani/one-nine-script/raw/main/onenineInstanceCreation.sh
-chmod +x bitnami-file-reset-cron.sh && chmod +x onenineInstanceCreation.sh
+wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-commands.sh
+chmod +x bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-commands.sh
 ./bitnami-file-reset-cron.sh > /dev/null 2>&1
 /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1 && /opt/bitnami/ctlscript.sh restart apache
