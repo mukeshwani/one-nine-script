0562d081d1aae64e74e53de148e2d0de2c6b82c2
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 14:05:12 -0500

    updated cron to be every 30 minutes

--- bitnami-file-reset-cron.sh
+++ bitnami-file-reset-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -l > crontab_new
-echo "30 0 * * * onenineInstanceCreation.sh" >> crontab_new
+echo "*/30 * * * * onenineInstanceCreation.sh" >> crontab_new
 crontab crontab_new
 rm crontab_new
