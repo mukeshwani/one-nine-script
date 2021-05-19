8f7787a7177e0623f465c8a579172196abda550f
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 17:17:40 -0500

    updated chown commands

--- shell-steps.sh
+++ shell-steps.sh
@@ -1,6 +1,6 @@
 mkdir /home/bitnami/oneninescripts && cd /home/bitnami/oneninescripts
 wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
 wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-commands.sh
-chown bitnami:daemon bitnami-file-reset-commands.sh && chown bitnami:daemon bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-commands.sh
+chown -R bitnami:daemon /home/bitnami/oneninescripts/ && chmod +x bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-commands.sh
 ./bitnami-file-reset-cron.sh > /dev/null 2>&1
 /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1 && /opt/bitnami/ctlscript.sh restart apache
