7b495711b19e3e8116f57bbeb2acca541e471ced
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 14:46:13 -0500

    path update

--- bitnami-file-reset-cron.sh
+++ bitnami-file-reset-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -l > crontab_new
-echo "*/5 * * * * /home/bitnami/oneninescrits/onenineInstanceCreation.sh >> /home/bitnami/oneninescripts/bitnami-file-reset-cron.log 2>&1" >> crontab_new
+echo "*/5 * * * * /home/bitnami/oneninescripts/onenineInstanceCreation.sh >> /home/bitnami/oneninescripts/bitnami-file-reset-cron.log 2>&1" >> crontab_new
 crontab crontab_new
 rm crontab_new
