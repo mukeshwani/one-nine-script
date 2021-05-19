71da1273e93e1e93e0b33dbcd9d004128d63c794
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 14:31:05 -0500

    changed wget with -q flag

--- shell-steps.sh
+++ shell-steps.sh
@@ -1,6 +1,6 @@
 mkdir /home/bitnami/oneninescripts && cd oneninescripts
-wget https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
-wget https://github.com/mukeshwani/one-nine-script/raw/main/onenineInstanceCreation.sh
+wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
+wget -q https://github.com/mukeshwani/one-nine-script/raw/main/onenineInstanceCreation.sh
 chmod +x bitnami-file-reset-cron.sh && chmod +x onenineInstanCreation.sh
 ./bitnami-file-reset.sh > /dev/null 2>&1
 /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1 && /opt/bitnami/ctlscript.sh restart apache
