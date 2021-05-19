466dd18be4bda334d825fdc36d1c178a4a73793d
Mukesh Wani <mukesh.wani@gmail.com>
2021-05-19 14:15:35 -0500

    updated to output cron to log file

--- bitnami-file-reset-cron.sh
+++ bitnami-file-reset-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 crontab -l > crontab_new
-echo "*/30 * * * * onenineInstanceCreation.sh" >> crontab_new
+echo "*/30 * * * * onenineInstanceCreation.sh >> /home/bitnami/bitnami-file-reset-cron.log 2>&1" >> crontab_new
 crontab crontab_new
 rm crontab_new
