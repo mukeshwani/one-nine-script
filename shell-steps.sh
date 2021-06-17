51a6afa43b168c8084f95bc58be3cc8335bb7438
Mukesh Wani <mukesh.wani@gmail.com>
2021-06-17 09:28:27 -0500

    reverted URL updates

--- shell-steps.sh
+++ shell-steps.sh
@@ -1,6 +1,6 @@
 mkdir /home/bitnami/oneninescripts && cd /home/bitnami/oneninescripts
-wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh?token=AAOJI4W6VNIZ26JNHLSIXZDAZNJOQ
-wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-commands.sh?token=AAOJI4U7HRZZYTOMRFODJ2LAZNJQO
+wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
+wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-commands.sh
 chown -R bitnami:bitnami /home/bitnami/oneninescripts/ && chmod +x bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-commands.sh
 ./bitnami-file-reset-cron.sh > /dev/null 2>&1
 /opt/bitnami/apps/wordpress/bnconfig --disable_banner 1 && /opt/bitnami/ctlscript.sh restart apache
