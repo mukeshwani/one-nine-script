469a70ebe4197ac8982097b80ee3f8387ed03342
Mukesh Wani <mukesh.wani@gmail.com>
2021-08-02 19:18:09 -0500

    Delete shell-steps.sh

--- shell-steps.sh
+++
@@ -1,6 +0,0 @@
-mkdir /home/bitnami/oneninescripts && cd /home/bitnami/oneninescripts
-wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-cron.sh
-wget -q https://raw.githubusercontent.com/mukeshwani/one-nine-script/main/bitnami-file-reset-commands.sh
-chown -R bitnami:bitnami /home/bitnami/oneninescripts/ && chmod +x bitnami-file-reset-cron.sh && chmod +x bitnami-file-reset-commands.sh
-./bitnami-file-reset-cron.sh > /dev/null 2>&1
-/opt/bitnami/apps/wordpress/bnconfig --disable_banner 1 && /opt/bitnami/ctlscript.sh restart apache
