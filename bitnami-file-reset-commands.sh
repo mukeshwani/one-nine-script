0a5744c0ad2efbc5fe54cc0b416137f0950aa983
Mukesh Wani <mukesh.wani@gmail.com>
2021-08-12 13:25:15 -0500

    Update bitnami-file-reset-commands.sh

--- bitnami-file-reset-commands.sh
+++ bitnami-file-reset-commands.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs/
-sudo find /opt/bitnami/apps/wordpress/htdocs/ -type d -exec chmod 0775 {} \;
-sudo find /opt/bitnami/apps/wordpress/htdocs/ -type f -exec chmod 0664 {} \;
+sudo chown -R bitnami:daemon /opt/bitnami/apps/wordpress/htdocs
+sudo find /opt/bitnami/apps/wordpress/htdocs -type d -exec chmod 775 {} \;
+sudo find /opt/bitnami/apps/wordpress/htdocs -type f -exec chmod 664 {} \;
