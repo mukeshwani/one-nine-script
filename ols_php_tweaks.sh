76517d6ba086644f528ed917668e8c1b7e5d2bfa
Mukesh Wani <mukesh.wani@gmail.com>
2022-01-25 15:52:45 -0600

    cleaned up file

--- ols_php_tweaks.sh
+++ ols_php_tweaks.sh
@@ -16,17 +16,4 @@ sed  -e '/upload_max_filesize/s/.*/upload_max_filesize = 200M/' \
 -e '/max_input_time/s/.*/max_input_time = 600/'  \
 -e "/memory_limit/s/.*/memory_limit = $memoryLimit/" \
 -i.bak $phpConfPath
-#sed  -e '/post_max_size/s/.*/post_max_size = 200m/' -i.bak $phpConfPath
 
-#add cgipassauth directive to allow passing the authentication headers to external API endpoints. This is only if the CGIPassAuth is not present in the file
-#grep -q -e 'CGIPassAuth' $wordpressVhostConfPath || sed '/^<Directory \"\/opt\/bitnami\/wordpress\">.*/a CGIPassAuth On' -i.bak $wordpressVhostConfPath
-#grep -q -e 'CGIPassAuth' $wordpressVhostHttpsConfPath || sed '/^<Directory \"\/opt\/bitnami\/wordpress\">.*/a CGIPassAuth On' -i.bak $wordpressVhostHttpsConfPath
-
-#in case CGIPassAuth is set to off then run this to turn in on.
-#sed '/CGIPassAuth Off/s/Off/On/g' -i.bak $wordpressVhostConfPath
-#sed '/CGIPassAuth Off/s/Off/On/g' -i.bak $wordpressVhostHttpsConfPath
-
-
-#disable pagespeed. This is usually not needed if using other means for optimizations or using a smaller instance.
-#sed -e '/Include conf\/pagespeed.conf/ s/^#*/#/' -i $httpdConfPath
-#sed -e '/Include conf\/pagespeed_libraries.conf/ s/^#*/#/' -i.bak $httpdConfPath
