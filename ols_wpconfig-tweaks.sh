e429e92c213ad8912d38cb6ab7bbe5822049275c
Mukesh Wani <mukesh.wani@gmail.com>
2022-02-02 10:53:29 -0600

    change wpconfig after wordpress install to make siteurl and home work on any ip or host

---
+++ ols_wpconfig-tweaks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#wpConfPath='wp-config.php'
+wpConfPath='wp-config.php'
+
+#if ( defined( 'WP_CLI' ) ) {
+#        $_SERVER['HTTP_HOST'] = '127.0.0.1';
+#}
+
+#define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/' );
+#define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/' );
+protocolCode="\$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';"
+
+sed -i "/.*That's all, stop editing! Happy publishing.*/i \\
+$protocolCode \\
+// Set SITEURL and HOME using a dynamic protocol. \\
+define('WP_SITEURL', \$protocol . '://' . \$_SERVER['HTTP_HOST']); \\
+define('WP_HOME', \$protocol . '://' . \$_SERVER['HTTP_HOST']); \\
+" $wpConfPath
