1a0c8d1617b3f5c4ffba5af4c1ec0a89aae2f51a
Mukesh Wani <mukesh.wani@gmail.com>
2022-04-14 16:18:37 -0500

    update env & auto load htaccess at server config

--- ols_tweaks.sh
+++ ols_tweaks.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-httpdConfPath='/usr/local/lsws/conf/httpd_config.conf'
+httpdConfPath='httpd_config.conf'
 
-grep -q -e 'autoLoadHtaccess' $httpdConfPath || sed '/^tuning.*{.*/iautoLoadHtaccess          1' -i.bak $httpdConfPath
+grep -q -e 'autoLoadHtaccess' $httpdConfPath || sed -e '/^tuning.*{.*/iautoLoadHtaccess          1' -i.bak $httpdConfPath
+sed -e '/^extprocessor.*lsphp.*{/,/^}/ s/maxConns.*/maxConns                35/' \
+-e '/^extprocessor.*lsphp.*{/,/^}/ s/PHP_LSAPI_CHILDREN.*/PHP_LSAPI_CHILDREN=35/' \
+-i.bak $httpdConfPath
+
+
